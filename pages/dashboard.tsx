d0633c03c2241ea10677f2a565414127e2b42f75
Angelo Filho <angelo.filho06@hotmail.com>
2022-11-09 22:59:30 -0300

    Add sign out

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,16 +1,12 @@
 import { useContext, useEffect } from "react";
 import Can from "../components/Can";
 import { AuthContext } from "../context/AuthContext";
-import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
-  const userCanSeeMetrics = useCan({
-    roles: ["editor"],
-  });
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api
@@ -22,6 +18,8 @@ export default function Dashboard() {
   return (
     <div>
       <h1>Dashboard: {user?.email} </h1>
+
+      <button onClick={signOut}>Sign Out</button>
 
       <Can roles={["administrator"]}>
         <div>Metrics</div>
